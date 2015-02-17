4a96a104c28a6a4f70a270af52abc2db4597c2b0
Joris Blaak <joris@label305.com>
2015-02-17 09:29:31 +0100

    Update provisioning.sh

--- provisioning.sh
+++ provisioning.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# NPM
+# NPM - Node Package Manager
 sudo apt-get install npm
 npm install -g http-server
 
-# GIMP
+# GIMP - when we want to be a designer
 sudo apt-get install gimp
 
-# PhantomJS
+# PhantomJS - browser for JS testing
 cd ~/Tools
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
 tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
@@ -17,3 +17,7 @@ mkdir $HOME/bin
 ln -s ~/Tools/phantomjs-1.9.7-linux-x86_64/bin/phantomjs $HOME/local/share/phantomjs 
 ln -s ~/Tools/phantomjs-1.9.7-linux-x86_64/bin/phantomjs $HOME/local/bin/phantomjs
 ln -s ~/Tools/phantomjs-1.9.7-linux-x86_64/bin/phantomjs $HOME/bin/phantomjs
+
+# Remove Apport - this is the thing that keeps telling you things have crashed, while you're not using them
+# I don't know if this should remain here, but for now, it is nice to not be bugged with errors
+sudo apt-get remove apport
