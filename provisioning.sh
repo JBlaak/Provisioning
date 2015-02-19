5b5d8a1ec972b881a7f730d382cab23cff0586f2
Joris Blaak <joris@label305.com>
2015-02-19 19:23:20 +0100

    Added installing PHP 5.6

--- provisioning.sh
+++ provisioning.sh
@@ -4,6 +4,11 @@
 sudo apt-get install npm
 npm install -g http-server
 
+# PHP 5.6
+sudo add-apt-repository ppa:ondrej/php5-5.6
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install php5
+
 # GCloud
 curl https://sdk.cloud.google.com | bash
 
