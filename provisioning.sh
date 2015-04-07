215a52c59a0b43ca492541ad1b8b3ff652d74097
Joris Blaak <joris@label305.com>
2015-04-07 19:38:00 +0200

    Added Spotify

--- provisioning.sh
+++ provisioning.sh
@@ -50,6 +50,12 @@ sudo add-apt-repository ppa:pinta-maintainers/pinta-stable
 sudo apt-get update
 sudo apt-get install pinta 
 
+# Spotify
+sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+sudo apt-get update -qq
+sudo apt-get install spotify-client
+
 # Shutter - screenshots
 sudo apt-get install shutter
 
