9e05aea0c030a654cd018d8c0c7fbe19002db814
Joris Blaak <joris@label305.com>
2015-06-01 12:10:20 +0200

    Added `-y` argument to all installations through apt-get

--- provisioning.sh
+++ provisioning.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # NPM - Node Package Manager
-sudo apt-get install npm
+sudo apt-get install -y npm
 npm install -g http-server
 
 # PHP 5.6
 sudo add-apt-repository ppa:ondrej/php5-5.6
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install php5
+sudo apt-get install -y php5
 
 # PHP Extensions
-sudo apt-get install php5-gd
-sudo apt-get install php5-mccrypt
-sudo apt-get install php5-curl
-sudo apt-get install php5-memcached
-sudo apt-get install php5-intl
-sudo apt-get install php5-mysql
-sudo apt-get install php5-xdebug
+sudo apt-get install -y php5-gd
+sudo apt-get install -y php5-mccrypt
+sudo apt-get install -y php5-curl
+sudo apt-get install -y php5-memcached
+sudo apt-get install -y php5-intl
+sudo apt-get install -y php5-mysql
+sudo apt-get install -y php5-xdebug
 
 # MySQL
-sudo apt-get install mysql-server
+sudo apt-get install -y mysql-server
 
 # Emma, for browsing MySQL databases
-sudo apt-get install emma
+sudo apt-get install -y emma
 
 # MySQL Workbech, when you want an overkill of MySQL function
-sudo apt-get install mysql-workbench
+sudo apt-get install -y mysql-workbench
 
 # Ruby
-sudo apt-get install ruby-full
+sudo apt-get install -y ruby-full
 
 # Gems
 sudo gem install sass
@@ -40,11 +40,11 @@ sudo mv composer.phar /usr/local/bin/composer
 # JDK
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java7-installer
-sudo apt-get install oracle-java8-installer
+sudo apt-get install -y oracle-java7-installer
+sudo apt-get install -y oracle-java8-installer
 
 # Virtual framebuffer, to run Selenium in
-sudo apt-get install xvfb
+sudo apt-get install -y xvfb
 
 # GCloud
 curl https://sdk.cloud.google.com | bash
@@ -53,39 +53,39 @@ curl https://sdk.cloud.google.com | bash
 sudo wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # Gitg for when we're lost in branches
-sudo apt-get install gitg
+sudo apt-get install -y gitg
 
 # Vagrant and Virtual box for VMs
-sudo apt-get install vagrant
-sudo apt-get install virtualbox
+sudo apt-get install -y vagrant
+sudo apt-get install -y virtualbox
 
 # POEdit for editing .po translation files
-sudo apt-get install poedit
+sudo apt-get install -y poedit
 
 # GIMP - when we want to be a designer
-sudo apt-get install gimp
+sudo apt-get install -y gimp
 
 # Pinta, for painting
 sudo add-apt-repository ppa:pinta-maintainers/pinta-stable
 sudo apt-get update
-sudo apt-get install pinta 
+sudo apt-get install -y pinta 
 
 # Spotify
 sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 sudo apt-get update -qq
-sudo apt-get install spotify-client
+sudo apt-get install -y spotify-client
 
 # Shutter - screenshots
-sudo apt-get install shutter
+sudo apt-get install -y shutter
 
 # Needed for stuff like Travis
-sudo apt-get install ruby-dev
+sudo apt-get install -y ruby-dev
 
 # f.lux
 sudo add-apt-repository ppa:kilian/f.lux
 sudo apt-get update
-sudo apt-get install fluxgui
+sudo apt-get install -y fluxgui
 
 # PhantomJS - browser for JS testing
 cd ~/Tools
