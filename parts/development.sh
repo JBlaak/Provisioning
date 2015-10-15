7023fc8737a5b883b69d68a1c439a65d78ec77c2
Joris Blaak <joris@label305.com>
2015-10-15 09:54:23 +0200

    Added `xvfb`

--- parts/development.sh
+++ parts/development.sh
@@ -5,6 +5,8 @@ sudo gem install sass
 
 sudo apt-get install -y filezilla
 
+sudo apt-get install -y xvfb
+
 # Some fonts
 wget --output-document=fonts.zip https://www.dropbox.com/sh/y4i4tkvbta48zxm/AAAvkcp3UQbf936T2dUi4D2Ga?dl=1
 unzip fonts.zip -d ~/.fonts
