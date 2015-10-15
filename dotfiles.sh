363068a9c5d2d0f3708aca1068a704ec31aa2e7c
Joris Blaak <joris@label305.com>
2015-10-15 11:00:16 +0200

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,5 +6,5 @@ git remote add origin git@github.com:JBlaak/.dotfiles.git
 git fetch
 git checkout -t origin/master
 
-sh -c "echo 'source ~/.bash_profile' >> ~/.bashrc"
-sh -c "echo '. ~/.env' >> ~/.bashrc"
+echo 'source ~/.bash_profile' >> ~/.bashrc
+echo '. ~/.env' >> ~/.bashrc
