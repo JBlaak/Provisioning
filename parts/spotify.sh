d863e3e78dee8f57b35b09a35bccbac400d08ec1
Joris Blaak <joris@label305.com>
2015-10-16 09:48:49 +0200

    Added `libgcrypt` for Spotify

--- parts/spotify.sh
+++ parts/spotify.sh
@@ -4,3 +4,7 @@ sudo sh -c "echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
 sudo apt-get update
 sudo apt-get install -y --force-yes spotify-client
+
+wget http://ftp.nl.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_amd64.deb
+sudo dpkg -i libgcrypt11_1.5.0-5+deb7u3_amd64.deb
+rm libgcrypt11_1.5.0-5+deb7u3_amd64.deb
