9f04a3eab1cc353f983f63abcdbf6cc94999b76d
Joris Blaak <joris@label305.com>
2015-10-17 16:21:00 +0200

    Increased `max_user_watches` so that PhpStorm doesn't nag about it

--- parts/development.sh
+++ parts/development.sh
@@ -2,10 +2,13 @@
 
 sudo apt-get install -y ruby-full 
 sudo gem install sass
-
 sudo apt-get install -y filezilla
 
+# Virtual frame buffer
 sudo apt-get install -y xvfb
+
+# For external file watching
+sudo sh -c "echo fs.inotify.max_user_watches = 524288 >> /etc/sysctl.conf"
 
 # Some fonts
 wget --output-document=fonts.zip https://www.dropbox.com/sh/y4i4tkvbta48zxm/AAAvkcp3UQbf936T2dUi4D2Ga?dl=1
