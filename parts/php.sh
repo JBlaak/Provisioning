451baa45c9f00672379f54d0ea4054cee389f9c3
Joris Blaak <joris@label305.com>
2016-01-12 11:08:49 +0100

    Installing imagick with PHP7.0

--- parts/php.sh
+++ parts/php.sh
@@ -11,6 +11,9 @@ sudo apt-get install -y php7.0-curl
 sudo apt-get install -y php7.0-intl
 sudo apt-get install -y php7.0-mysql
 
+# https://gist.github.com/nivv/5d9a12af5472b91606e6
+sudo apt-get install -y pkg-config libmagickwand-dev imagemagick build-essential
+
 sudo sh -c "echo 'xdebug.max_nesting_level=500' >> /etc/php5/cli/conf.d/20-xdebug.ini"
 
 sudo php5enmod mcrypt
