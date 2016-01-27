816420ad4661ac26f7fdc10386330afd88b1faed
Joris Blaak <joris@label305.com>
2016-01-27 08:55:59 +0100

    Added `nvm`

--- parts/node.sh
+++ parts/node.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# NodeJS
 curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
 sudo apt-get install -y nodejs
+
+# NVM (Node version manager)
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+
