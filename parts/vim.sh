d3739f079034a4b118086d65d01ced9820732870
Joris Blaak <joris@label305.com>
2016-04-07 10:06:51 +0200

    Update vim.sh

--- parts/vim.sh
+++ parts/vim.sh
@@ -5,12 +5,6 @@ sudo apt-get install -y vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-# YouCompleteMe requires some making
-sudo apt-get install -y build-essential cmake
-sudo apt-get install -y python-dev
-cd ~/.vim/bundle/YouCompleteMe
-./install.py
-
 # Tern requires npm install
 cd ~/.vim/bundle/tern_for_vim
 npm install
