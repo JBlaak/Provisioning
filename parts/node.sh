7c79a7a9e464dd5bb96fedc23217e10a10600ecc
Joris Blaak <joris@label305.com>
2016-04-28 15:36:52 +0200

    Added eslint

--- parts/node.sh
+++ parts/node.sh
@@ -2,3 +2,6 @@
 
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# eslint
+npm install -g eslint eslint-config-standard eslint-plugin-standard eslint-plugin-promise
