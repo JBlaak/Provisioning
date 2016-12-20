758011ffe8ab09e8a8d6c79bc892ad20b294b2de
Joris Blaak <joris@label305.com>
2016-12-20 10:49:27 +0100

    Installation of mysql-workbench 6.38

--- parts/mysql.sh
+++ parts/mysql.sh
@@ -7,4 +7,9 @@ sudo apt install -y mysql-server
 sudo apt install -y emma
 
 # MySQL Workbech, when you want an overkill of MySQL function
-sudo apt install -y mysql-workbench
+wget http://nl.archive.ubuntu.com/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
+sudo dpkg -i libpng12-0_1.2.54-1ubuntu1_amd64.deb
+rm libpng12-0_1.2.54-1ubuntu1_amd64.deb
+wget http://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community-6.3.8-1ubu1604-amd64.deb
+sudo dpkg -i mysql-workbench-community-6.3.8-1ubu1604-amd64.deb
+rm mysql-workbench-community-6.3.8-1ubu1604-amd64.deb
