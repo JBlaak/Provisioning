9a4f67fa35da28830b6baa88f9632038844c80b0
Joris Blaak <joris@label305.com>
2017-01-05 15:38:06 +0100

    Added libgstreamer

--- parts/stuffs.sh
+++ parts/stuffs.sh
@@ -3,6 +3,10 @@ sudo apt install -y lib32ncurses5
 sudo apt install -y lib32stdc++6 
 sudo apt install -y libappindicator1 libindicator7
 
+wget http://fr.archive.ubuntu.com/ubuntu/pool/main/g/gst-plugins-base0.10/libgstreamer-plugins-base0.10-0_0.10.36-1_amd64.deb
+wget http://fr.archive.ubuntu.com/ubuntu/pool/universe/g/gstreamer0.10/libgstreamer0.10-0_0.10.36-1.5ubuntu1_amd64.deb
+sudo dpkg -i libgstreamer*.deb
+
 sudo apt install -y curl
 
 sudo add-apt-repository ppa:graphics-drivers/ppa
