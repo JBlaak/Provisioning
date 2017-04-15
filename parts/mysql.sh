3b156327bf34834155cfdfbbaaa97c0d7ef2e31a
Joris Blaak <joris@label305.com>
2017-04-15 16:09:28 +0200

    Upgrade to Mysql Workbench 6.3.9

--- parts/mysql.sh
+++ parts/mysql.sh
@@ -6,10 +6,7 @@ sudo apt install -y mysql-server
 # Emma, for browsing MySQL databases
 sudo apt install -y emma
 
-# MySQL Workbech, when you want an overkill of MySQL function
-wget http://nl.archive.ubuntu.com/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
-sudo dpkg -i libpng12-0_1.2.54-1ubuntu1_amd64.deb
-rm libpng12-0_1.2.54-1ubuntu1_amd64.deb
-wget http://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community-6.3.8-1ubu1604-amd64.deb
-sudo dpkg -i mysql-workbench-community-6.3.8-1ubu1604-amd64.deb
-rm mysql-workbench-community-6.3.8-1ubu1604-amd64.deb
+# MySQL Workbech
+wget https://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community-6.3.9-1ubuntu16.10-amd64.deb
+sudo dpkg -i mysql-workbench-community-6.3.9-1ubuntu16.10-amd64.deb
+rm mysql-workbench-community-6.3.9-1ubuntu16.10-amd64.deb
