e976cde112fcbd32783c96ee4bbcf4fa8c5c386c
Joris Blaak <joris@label305.com>
2017-04-15 16:11:37 +0200

    Source .bashrc after nvm installation

--- parts/node.sh
+++ parts/node.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+source ~/.bashrc
+
 nvm install v7.2.0
+source ~/.bashrc
 
 # yarn
 npm install -g yarn
