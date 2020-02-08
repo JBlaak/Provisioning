bae074d031c5d79e052e6f0583bee54e868f68b5
Joris Blaak <joris@label305.com>
2020-02-08 14:05:21 +0100

    Use proper apt repository for mysql-server installation

--- parts/mysql.sh
+++ parts/mysql.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# MySQL Apt repository
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.14-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.14-1_all.deb
+sudo apt update
+rm mysql-apt-config_0.8.14-1_all.deb
+
 # MySQL
 sudo apt install mysql-server
 
@@ -7,9 +13,5 @@ sudo apt install mysql-server
 sudo apt install -y emma
 
 # MySQL Workbech
-wget https://repo.mysql.com//mysql-apt-config_0.8.14-1_all.deb
-sudo dpkg -i mysql-apt-config_0.8.14-1_all.deb
-sudo apt update
 sudo apt install -y mysql-workbench-community
-rm mysql-apt-config_0.8.14-1_all.deb
 
