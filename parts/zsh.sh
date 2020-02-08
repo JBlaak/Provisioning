ffac4e7250e7e0d974508581d8c5a53576c30d1b
Joris Blaak <joris@label305.com>
2020-02-08 14:41:28 +0100

    Use proper url to fetch oh-my-zsh

--- parts/zsh.sh
+++ parts/zsh.sh
@@ -1,4 +1,5 @@
 sudo apt install -y zsh 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
