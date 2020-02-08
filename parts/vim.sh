58a60012f53c89fd277a1ceb0d893798db4f62f2
Joris Blaak <joris@label305.com>
2020-02-08 15:17:56 +0100

    Added installation of vim-notes

--- parts/vim.sh
+++ parts/vim.sh
@@ -5,4 +5,5 @@ sudo apt install -y vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-
+git clone https://github.com/xolox/vim-misc.git ~/.vim/bundle
+git clone https://github.com/xolox/vim-notes.git ~/.vim/bundle
